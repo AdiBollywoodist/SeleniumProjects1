bc06a1b3f17b7b7e1c762533d57b5e26eb6c33b2
ADITYA <ADITYA@LAPTOP-G83G994S>
2023-08-11 02:56:16 +0530

    Try 2

---
+++ src/main/java/SeleniumProjects/SeleniumProjects1/App.java
@@ -0,0 +1,13 @@
+package SeleniumProjects.SeleniumProjects1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
